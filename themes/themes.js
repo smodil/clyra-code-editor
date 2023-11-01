1cc47a903fd64e4927da5531de12cf9fc5d3a6d5
smodil <149347603+smodil@users.noreply.github.com>
2023-11-01 15:28:55 +0530

    Add files via upload

---
+++ themes/themes.js
@@ -0,0 +1,22 @@
+
+
+// var theme = ClyraCodeEditorTheme;
+// theme.TitleBar(BackgroundColor("Black"));
+// theme.TitleBar(TextColor("Red"));
+// theme.TitleBar(TextShadow(0, 0, 5, "red"));
+// theme.TitleBar(BoxShadow(3, 3, 10, "red"));
+// theme.TitleBar(Cursor("progress"));
+
+// theme.CodeEditorPanel(BackgroundColor("black"));
+// theme.CodeEditorPanel(TextColor("red"));
+// theme.CodeEditorPanel(TextShadow(0, 0, 5, "red"));
+// theme.EditorWindow(BackgroundColor("black"));
+// theme.Button_Hover(BackgroundColor("darkred"));
+// theme.Button_Press(BackgroundColor("orangered"));
+// theme.Button_Pressed(BackgroundColor("orange"));
+
+// theme.Button(BackgroundColor("red"));
+// var code_ = ClyraCodeEditor;
+// code_.setDefaultCode("Thanks for using this theme!!!! 2023 Copyright(c) operatingSyste");
+
+// ClyraThemeInfo("RedFont", "OperatingSyste")
